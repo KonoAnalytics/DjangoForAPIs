0c1180026f217a852895778e75d255a38b407cdc
Jonathan Bennett <jonathan@konoanalytics.com>
2019-10-25 21:24:56 -0500

    Finished Chapter 8: Viewsets and Routers

--- BlogApp/posts/urls.py
+++ BlogApp/posts/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
+
+# Routes replace the following code
+# p.153 Django for APIs
+# from django.urls import path
+# from .views import PostList, PostDetail, UserList, UserDetail
+#
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('<int:pk>/', PostDetail.as_view()),
+#     path('', PostList.as_view()),
+# ]
